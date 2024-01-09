5cb70542308ee53dcba8b2b58294ac4456f8edd3
nidsa <147480064+nidsa@users.noreply.github.com>
2024-01-09 07:50:35 +0700

    Add files via upload

---
+++ If-else_Example/src/Test1.java
@@ -0,0 +1,14 @@
+public class Test1 {
+    public static void main(String[] args){
+        int age = 25;
+        if(age<10){
+            System.out.println("She is younger");
+        }
+        else if(age < 20 ){
+            System.out.println("She is teenager");
+        }
+        else {
+            System.out.println("She is adult");
+        }
+    }
+}
